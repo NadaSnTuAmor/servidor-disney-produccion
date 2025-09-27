ccacab19290869fcb738ea8e839682dd7cd09e06
NadaSnTuAmor <nadasntuamor@gmail.com>
2025-09-26 22:02:43 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -87,7 +87,7 @@ function handleDemoLogin(username, password) {
     // ⭐ USUARIOS BASADOS EN TU GOOGLE SHEETS
     const demoUsers = {
         // ⭐ ADMIN
-        'NadaSnTuAmor_2025': { 
+        'admin': { 
             password: 'DisneyShield#Ultra$ecure!2025', 
             type: USER_TYPES.ADMIN, 
             name: 'Administrador',
