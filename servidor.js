aa00c8cee942bfeaac5b3ff7381d6c212e982758
Victor Bohorquez <viromi.global@gmail.com>
2025-10-03 19:10:06 -0500

    fix: actualiza URLs a www.nadasntuamor.com para producción1

--- servidor.js
+++ servidor.js
@@ -662,12 +662,14 @@ function iniciarVigilanciaEmail(email) {
   console.log(`✅ Vigilancia configurada para ${email} - ${timers.length} timers activos`);
 }
 
-//app.use(cors({
+/*
+app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
+*/
 
 app.use(cors({
   origin: ['https://web.nadasntuamor.com'],
